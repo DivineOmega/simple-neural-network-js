75fc65260e9cd37d0df873d7dcd269635201dc40
Jordan Hall <jordan@hall05.co.uk>
2017-08-19 20:55:47 +0100

    Correct call to random method

--- js/NeuralNetwork/Neuron.js
+++ js/NeuralNetwork/Neuron.js
@@ -7,7 +7,7 @@ class Neuron
 				
 		for (var i=0; i<numInputs+1; ++i)
 		{
-			var newWeight = -1 + (Math.random*2);
+			var newWeight = -1 + (Math.random()*2);
 			
 			this.weights.push(newWeight);
 		}
